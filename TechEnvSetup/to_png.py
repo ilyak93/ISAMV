b18632989365354ccdfabe463f31afd0cce1c777
ilyak93 <ilyak93@gmail.com>
2022-08-09 15:34:41 +0300

    translating bytes to png
    - a script which translates the bytes sequences to many color, depth and thermal images

---
+++ TechEnvSetup/to_png.py
@@ -0,0 +1,131 @@
+import os
+from PIL import Image
+import io
+import numpy as np
+
+def main():
+    for i in range(1, 2):
+        g_path = "G:/Vista_project/fusion/" +str(i) + "/"
+        left_path = g_path + "/left/"
+        right_path = g_path + "/right/"
+        rs_path = g_path + "/rs/"
+        if not os.path.exists(left_path):
+            os.mkdir(left_path)
+        if not os.path.exists(right_path):
+            os.mkdir(right_path)
+        if not os.path.exists(rs_path):
+            os.mkdir(rs_path)
+
+
+        files = os.listdir(g_path)[:-3]
+        files = [f for f in files if len(f.split('.'))>1 and f.split('.')[1] == "bin"]
+        left = [f for f in files if "tc1" in f and "ts" not in f]
+        right = [f for f in files if "tc2" in f and "ts" not in f]
+        rs_color = [f for f in files if "color" in f and "ts" not in f]
+        rs_depth = [f for f in files if "depth" in f and "ts" not in f]
+
+        left_ts = [f for f in files if "tc1" in f and "ts" in f]
+        right_ts = [f for f in files if "tc2" in f and "ts" in f]
+        rs_color_ts = [f for f in files if "color" in f and "ts" in f]
+        rs_depth_ts = [f for f in files if "depth" in f and "ts" in f]
+
+        img_size = 640*512*2
+        ts_size = 8
+        name = 0
+        prev = 0
+
+        for l, r, lts, rts in zip(left,right, left_ts, right_ts):
+            with open(g_path+l, "rb") as left_bytes_stream,\
+                    open(g_path+r, "rb") as right_bytes_stream,\
+                    open(g_path + lts, "rb") as left_ts_bytes_stream,\
+                    open(g_path + rts, "rb") as right_ts_bytes_stream :
+
+                while(True):
+                    left_img_bytes = left_bytes_stream.read(img_size)
+                    right_img_bytes = right_bytes_stream.read(img_size)
+                    left_img_ts_bytes = left_ts_bytes_stream.read(ts_size)
+                    right_img_ts_bytes = right_ts_bytes_stream.read(ts_size)
+                    if len(left_img_bytes) != img_size or \
+                        len(right_img_bytes) != img_size or \
+                        len(left_img_ts_bytes) != ts_size or \
+                        len(right_img_ts_bytes) != ts_size :
+                        break
+                    left_stream = io.BytesIO(left_img_bytes)
+                    bytes = left_stream.getbuffer()
+                    left_img_np = np.frombuffer(bytes, dtype='uint16')
+                    left_img_np_norm = (left_img_np - np.min(left_img_np)) / (np.max(left_img_np) - np.min(left_img_np))
+                    left_img_np_norm = (left_img_np_norm * 65536).astype(dtype='uint16')
+                    left_img_np = left_img_np_norm.reshape((512, 640))
+                    left_img = Image.fromarray(left_img_np)
+
+                    left_ts_stream = io.BytesIO(left_img_ts_bytes)
+                    ts_bytes = left_ts_stream.getbuffer()
+                    left_img_ts_np = np.frombuffer(ts_bytes, dtype='longlong')
+                    ts = left_img_ts_np.item()
+                    cur = ts
+                    assert prev < cur
+                    prev = cur
+                    left_img.save(left_path + str(name)+'_' + str(ts) + ".png", format='PNG')
+
+                    right_stream = io.BytesIO(right_img_bytes)
+                    bytes = right_stream.getbuffer()
+                    right_img_np = np.frombuffer(bytes, dtype='uint16')
+                    right_img_np_norm = (right_img_np - np.min(right_img_np)) / (np.max(right_img_np) - np.min(right_img_np))
+                    right_img_np_norm = (right_img_np_norm * 65536).astype(dtype='uint16')
+                    right_img_np = right_img_np_norm.reshape((512, 640))
+                    right_img = Image.fromarray(right_img_np)
+
+                    right_ts_stream = io.BytesIO(right_img_ts_bytes)
+                    ts_bytes = right_ts_stream.getbuffer()
+                    right_img_ts_np = np.frombuffer(ts_bytes, dtype='longlong')
+                    ts = right_img_ts_np.item()
+                    right_img.save(right_path + str(name) + '_' + str(ts) + ".png", format='PNG')
+
+                    name = name + 1
+
+        color_img_size = 1280 * 720 * 3
+        depth_img_size = 1280 * 720 * 2
+
+        name = 0
+
+        for color, depth, color_ts in zip(rs_color, rs_depth, rs_color_ts):
+            with open(g_path + color, "rb") as color_bytes_stream, \
+                    open(g_path + depth, "rb") as depth_bytes_stream, \
+                    open(g_path + color_ts, "rb") as color_ts_bytes_stream :
+                while (True):
+                    color_img_bytes = color_bytes_stream.read(color_img_size)
+                    depth_img_bytes = depth_bytes_stream.read(depth_img_size)
+                    color_img_ts_bytes = color_ts_bytes_stream.read(ts_size)
+
+                    if len(color_img_bytes) != color_img_size or \
+                            len(depth_img_bytes) != depth_img_size or \
+                            len(color_img_ts_bytes) != ts_size:
+                        break
+
+                    color_ts_stream = io.BytesIO(color_img_ts_bytes)
+                    ts_bytes = color_ts_stream.getbuffer()
+                    color_img_ts_np = np.frombuffer(ts_bytes, dtype='longlong')
+                    ts = color_img_ts_np.item()
+
+                    color_stream = io.BytesIO(color_img_bytes)
+                    bytes = color_stream.getbuffer()
+                    color_img_np = np.frombuffer(bytes, dtype='uint8').reshape((720, 1280, 3))
+                    color_img = Image.fromarray(color_img_np)
+                    color_img.save(rs_path + str(name) + '_' + str(ts) + "color.png", format='PNG')
+
+                    depth_stream = io.BytesIO(depth_img_bytes)
+                    bytes = depth_stream.getbuffer()
+                    depth_img_np = np.frombuffer(bytes, dtype='uint16').reshape((720, 1280))
+                    right_img = Image.fromarray(depth_img_np)
+                    right_img.save(rs_path + str(name) + '_' + str(ts) + "depth.png", format='PNG')
+
+                    name = name + 1
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
