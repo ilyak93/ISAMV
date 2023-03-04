5571e078a89d858e5495b097d8c1310aaf406648
ilyak93 <ilyak93@gmail.com>
2023-03-04 04:23:55 +0200

    bug fix
    - vector<long long> didn't work properly, 0 as timestamps were written.
    - changed to pre allocated mapped files as with the rs &thermal images and depth
    - fixed totally the wrong allocation
    - looked like it also smoothed the pass between records and made it more continuous.

--- TechEnvSetup/main_v2.cpp
+++ TechEnvSetup/main_v2.cpp
@@ -41,20 +41,20 @@ class RSCallback {
 public:
     char** color_mfd_ptr = new char*;
     char** depth_mfd_ptr = new char*;
-    vector<long long int>& color_depth_ts;
+    char** color_depth_ts_mfd_ptr = new char*;
     volatile int& idx_depth;
     std::atomic<volatile int>& idx_color;
     std::mutex& mux;
     boost::shared_mutex& shared_mux;
 
 public:
-    RSCallback(char* cmfd_ptr, char* dmfd_ptr, vector<long long int>& ccdts,
+    RSCallback(char* cmfd_ptr, char* dmfd_ptr, char* cdtsmfd_ptr,
                std::atomic<volatile int>& idxc, int& idxd, std::mutex& mx,
                boost::shared_mutex& smx) :
-               idx_color(idxc), idx_depth(idxd),
-               color_depth_ts(ccdts), mux(mx), shared_mux(smx) {
+               idx_color(idxc), idx_depth(idxd), mux(mx), shared_mux(smx) {
         *color_mfd_ptr = cmfd_ptr;
         *depth_mfd_ptr = dmfd_ptr;
+        *color_depth_ts_mfd_ptr = cdtsmfd_ptr;
 
 
     }
@@ -69,7 +69,6 @@ public:
             long long int loc_ts =
                     std::chrono::duration_cast<std::chrono::nanoseconds>(
                                             now.time_since_epoch()).count();
-            this->color_depth_ts.push_back(loc_ts);
             for (const rs2::frame f: fs) {
                 auto vf = f.as<rs2::video_frame>();
                 if (vf.get_bytes_per_pixel() == 2) {
@@ -79,8 +78,12 @@ public:
                     idx_depth++;
                 } else {
                     size_t sz = vf.get_data_size();
-+                    memcpy((void *) ((uint8_t *) (*color_mfd_ptr) +
+                    memcpy((void *) ((uint8_t *) (*color_mfd_ptr) +
                                     idx_color * sz), vf.get_data(), sz);
+                    size_t ts_sz = sizeof(long long int);
+                    const char* loc_ts_bytes = static_cast<const char*>(static_cast<const void*>(&loc_ts));
+                    memcpy((void *) ((uint8_t *) (*color_depth_ts_mfd_ptr) +
+                    idx_color * ts_sz), loc_ts_bytes, ts_sz);
                     idx_color.fetch_add(1, std::memory_order_relaxed);
                 }
             }
@@ -92,18 +95,19 @@ public:
 class TC1Callback {
 public:
     char** tc_mfd_ptr = new char*;
-    vector<long long int> &tc_ts;
+    char** tc_ts_mfd_ptr = new char*;
     int& idx_tc;
     size_t sz;
     std::mutex &mux;
     boost::shared_mutex &shared_mux;
 
 public:
-    TC1Callback(char *tcm_ptr, vector<long long int> &tcts, int& ixtc,
+    TC1Callback(char *tcm_ptr, char *ttsm_ptr, int& ixtc,
                 size_t tc_size, std::mutex &mx, boost::shared_mutex &smx) :
-             tc_ts(tcts), idx_tc(ixtc), sz(tc_size),
+                idx_tc(ixtc), sz(tc_size),
             mux(mx), shared_mux(smx) {
         *tc_mfd_ptr =  tcm_ptr;
+        *tc_ts_mfd_ptr = ttsm_ptr;
     }
 
     // This operator overloading enables calling
@@ -115,7 +119,10 @@ public:
         const std::chrono::time_point <std::chrono::steady_clock> now = high_resolution_clock::now();
         long long int loc_ts = std::chrono::duration_cast<std::chrono::nanoseconds>(
                 now.time_since_epoch()).count();
-        tc_ts.push_back(loc_ts);
+        size_t ts_sz = sizeof(long long int);
+        const char* loc_ts_bytes = static_cast<const char*>(static_cast<const void*>(&loc_ts));
+        memcpy((void *) ((uint8_t *) (*tc_ts_mfd_ptr) +
+        idx_tc * ts_sz), loc_ts_bytes, ts_sz);
         idx_tc++;
     }
 };
@@ -123,18 +130,19 @@ public:
 class TC2Callback {
 public:
     char** tc_mfd_ptr = new char*;
-    vector<long long int> &tc_ts;
+    char** tc_ts_mfd_ptr = new char*;
     int& idx_tc;
     size_t sz;
     std::mutex &mux;
     boost::shared_mutex &shared_mux;
 
 public:
-    TC2Callback(char *tcm_ptr, vector<long long int> &tcts, int& ixtc,
+    TC2Callback(char *tcm_ptr, char *ttsm_ptr, int& ixtc,
                 size_t tc_size, std::mutex &mx, boost::shared_mutex &smx) :
-            tc_ts(tcts), idx_tc(ixtc), sz(tc_size),
+            idx_tc(ixtc), sz(tc_size),
             mux(mx), shared_mux(smx) {
         *tc_mfd_ptr =  tcm_ptr;
+        *tc_ts_mfd_ptr = ttsm_ptr;
     }
 
     // This operator overloading enables calling
@@ -146,7 +154,10 @@ public:
         const std::chrono::time_point <std::chrono::steady_clock> now = high_resolution_clock::now();
         long long int loc_ts = std::chrono::duration_cast<std::chrono::nanoseconds>(
                 now.time_since_epoch()).count();
-        tc_ts.push_back(loc_ts);
+        size_t ts_sz = sizeof(long long int);
+        const char* loc_ts_bytes = static_cast<const char*>(static_cast<const void*>(&loc_ts));
+        memcpy((void *) ((uint8_t *) (*tc_ts_mfd_ptr) +
+                idx_tc * ts_sz), loc_ts_bytes, ts_sz);
         idx_tc++;
     }
 };
@@ -157,20 +168,22 @@ public:
     mapped_file& depth_mapped_fd;
     mapped_file& tc1_mapped_fd;
     mapped_file& tc2_mapped_fd;
-    vector<long long int>& color_depth_ts;
-    vector<long long int>& tc1_ts;
-    vector<long long int>& tc2_ts;
+    mapped_file& cd_ts_mapped_fd;
+    mapped_file& tc1_ts_mapped_fd;
+    mapped_file& tc2_ts_mapped_fd;
     int idx;
     string save_dir;
 
 public:
     SaveCallback(mapped_file& cmfd, mapped_file& dmfd, mapped_file& tc1fd,
-                 mapped_file& tc2fd, vector<long long int>& cdts,
-                 vector<long long int>& tc1ts, vector<long long int>& tc2ts,
-                 int ix, string sdir) : color_mapped_fd(cmfd), depth_mapped_fd(dmfd),
-                           tc1_mapped_fd(tc1fd), color_depth_ts(cdts),
-                           tc1_ts(tc1ts), tc2_ts(tc2ts), tc2_mapped_fd(tc2fd),
-                           idx(ix), save_dir(sdir) {}
+                 mapped_file& tc2fd, mapped_file& cdtsm,
+                 mapped_file& t1tsm, mapped_file& t2tsm,
+                 int ix, string sdir) : color_mapped_fd(cmfd),
+                 depth_mapped_fd(dmfd), tc1_mapped_fd(tc1fd),
+                 tc2_mapped_fd(tc2fd),
+                 cd_ts_mapped_fd(cdtsm),
+                 tc1_ts_mapped_fd(t1tsm), tc2_ts_mapped_fd(t2tsm), idx(ix),
+                 save_dir(sdir) {}
 
 
     // This operator overloading enables calling
@@ -180,52 +193,9 @@ public:
         depth_mapped_fd.close();
         tc1_mapped_fd.close();
         tc2_mapped_fd.close();
-
-        ofstream cd_fout;
-        string color_depth_ts_name = save_dir + to_string(idx) + "color_depth_ts.bin";
-        cd_fout.open(color_depth_ts_name, ios::binary | ios::out);
-        cd_fout.write((char *) color_depth_ts.data(),
-                      color_depth_ts.size() * sizeof(long long int));
-        cd_fout.close();
-
-        ofstream tc1_fout;
-        string tc1_ts_name = save_dir + to_string(idx) + "tc1_ts.bin";
-        tc1_fout.open(tc1_ts_name, ios::binary | ios::out);
-        tc1_fout.write((char *) tc1_ts.data(),
-                      tc1_ts.size() * sizeof(long long int));
-        tc1_fout.close();
-
-        ofstream tc2_fout;
-        string tc2_ts_name = save_dir + to_string(idx) + "tc2_ts.bin";
-        tc2_fout.open(tc2_ts_name, ios::binary | ios::out);
-        tc2_fout.write((char *) tc2_ts.data(),
-                       tc2_ts.size() * sizeof(long long int));
-        tc2_fout.close();
-
-        /*
-         //this code check if the timesstamps are ordered
-        vector<long long int> v_sorted(tc1_ts.size());
-        partial_sort_copy(begin(tc1_ts), end(tc1_ts), begin(v_sorted), end(v_sorted));
-        vector<long long int> vs(v_sorted);
-        std::vector<long long int> diff;
-        std::set_difference(v_sorted.begin(), v_sorted.end(), tc1_ts.begin(), tc1_ts.end(),
-                            std::inserter(diff, diff.begin()));
-        cout << accumulate(diff.begin(), diff.end(), 0) << endl;
-
-        ofstream tc2_fout;
-        string tc2_ts_name = save_dir + to_string(idx) + "tc2_ts.bin";
-        tc2_fout.open(tc2_ts_name, ios::binary | ios::out);
-        tc2_fout.write((char *) tc2_ts.data(),
-                       tc2_ts.size() * sizeof(long long int));
-        tc2_fout.close();
-
-        vector<long long int> v_sorted2(tc2_ts.size());
-        partial_sort_copy(begin(tc2_ts), end(tc2_ts), begin(v_sorted2), end(v_sorted2));
-        std::vector<long long int> diff2;
-        std::set_difference(v_sorted2.begin(), v_sorted2.end(), tc2_ts.begin(), tc2_ts.end(),
-                            std::inserter(diff, diff2.begin()));
-        cout << accumulate(diff2.begin(), diff2.end(), 0) << endl;
-        */
+        cd_ts_mapped_fd.close();
+        tc1_ts_mapped_fd.close();
+        tc2_ts_mapped_fd.close();
     }
 };
 
@@ -444,15 +414,20 @@ int main() {
 
     long long color_size = 720LL * 1280 * rgb_ch * rs_fps * time_to_record;
     long long depth_size = 720LL * 1280 * depth_px_sz * rs_fps * time_to_record;
-
+    long long cd_ts_size = sizeof(long long int) * rs_fps * time_to_record;
+    long long tc_ts_size = sizeof(long long int) * tc_fps * time_to_record;
     //number of partitions which gives:
     // total time of recording = number_of_records * time_to_record
-    int number_of_records = 1;
+    int number_of_records = 5;
 
-    vector <vector<long long int>> HT1_tss_vec(number_of_records);
-    vector <vector<long long int>> HT2_tss_vec(number_of_records);
+    char **cd_ts_mfd_ptrs = (char **) new char *[number_of_records];
+    mapped_file * cd_ts_mapped_fds = (mapped_file * ) new mapped_file[number_of_records];
 
-    vector <vector<long long int>> color_depth_tss(number_of_records);
+    char **tc1_ts_mfd_ptrs = (char **) new char *[number_of_records];
+    mapped_file * tc1_ts_mapped_fds = (mapped_file * ) new mapped_file[number_of_records];
+
+    char **tc2_ts_mfd_ptrs = (char **) new char *[number_of_records];
+    mapped_file * tc2_ts_mapped_fds = (mapped_file * ) new mapped_file[number_of_records];
 
     char **tc1_mfd_ptrs = (char **) new char *[number_of_records];
     mapped_file * tc1_mapped_fds = (mapped_file * ) new mapped_file[number_of_records];
@@ -467,6 +442,36 @@ int main() {
     mapped_file * depth_mapped_fds = (mapped_file * ) new mapped_file[number_of_records];
 
     for (int l = 0; l < number_of_records; ++l) {
+
+        string cd_ts_file_path = save_dir + to_string(l) + "cd_ts.bin";
+        const char *cd_ts_FileName = cd_ts_file_path.c_str();
+        const std::size_t ColorDepthTsFileSize = cd_ts_size;
+
+        mapped_file_params params_cd_ts(cd_ts_FileName);
+        params_cd_ts.new_file_size = ColorDepthTsFileSize;
+        params_cd_ts.flags = mapped_file_base::readwrite;
+        cd_ts_mapped_fds[l] = mapped_file(params_cd_ts);
+        cd_ts_mfd_ptrs[l] = cd_ts_mapped_fds[l].data();
+
+        string t1_ts_file_path = save_dir + to_string(l) + "t1_ts.bin";
+        const char *t1_ts_FileName = t1_ts_file_path.c_str();
+        const std::size_t T1TsFileSize = tc_ts_size;
+
+        mapped_file_params params_t1_ts(t1_ts_FileName);
+        params_t1_ts.new_file_size = T1TsFileSize;
+        params_t1_ts.flags = mapped_file_base::readwrite;
+        tc1_ts_mapped_fds[l] = mapped_file(params_t1_ts);
+        tc1_ts_mfd_ptrs[l] = tc1_ts_mapped_fds[l].data();
+
+        string t2_ts_file_path = save_dir + to_string(l) + "t2_ts.bin";
+        const char *t2_ts_FileName = t2_ts_file_path.c_str();
+        const std::size_t T2TsFileSize = tc_ts_size;
+
+        mapped_file_params params_t2_ts(t2_ts_FileName);
+        params_t2_ts.new_file_size = T2TsFileSize;
+        params_t2_ts.flags = mapped_file_base::readwrite;
+        tc2_ts_mapped_fds[l] = mapped_file(params_t2_ts);
+        tc2_ts_mfd_ptrs[l] = tc2_ts_mapped_fds[l].data();
 
         string tc1_file_path = save_dir + to_string(l) + +"tc1.bin";
         const char *tc1_FileName = tc1_file_path.c_str();
@@ -515,14 +520,14 @@ int main() {
     std::mutex tc1_mutex;
     int idx_tc1 = 0;
 
-    auto tc1_callback = TC1Callback(tc1_mfd_ptrs[0], HT1_tss_vec[0], idx_tc1,
+    auto tc1_callback = TC1Callback(tc1_mfd_ptrs[0], tc1_ts_mfd_ptrs[0], idx_tc1,
                                     tc_size, tc1_mutex, shared_mux);
 
 
     std::mutex tc2_mutex;
     int idx_tc2 = 0;
 
-    auto tc2_callback = TC2Callback(tc2_mfd_ptrs[0], HT2_tss_vec[0], idx_tc2,
+    auto tc2_callback = TC2Callback(tc2_mfd_ptrs[0], tc2_ts_mfd_ptrs[0], idx_tc2,
                                     tc_size, tc2_mutex, shared_mux);
 
 
@@ -539,7 +544,7 @@ int main() {
     std::atomic<volatile int> idx_color(0);
     int idx_depth = 0;
     auto rs_callback = RSCallback(color_mfd_ptrs[0], depth_mfd_ptrs[0],
-                                  color_depth_tss[0], idx_color, idx_depth, mux,
+                                  cd_ts_mfd_ptrs[0], idx_color, idx_depth, mux,
                                   shared_mux);
 
 
@@ -597,9 +602,9 @@ int main() {
                                               depth_mapped_fds[cur_idx],
                                               tc1_mapped_fds[cur_idx],
                                               tc2_mapped_fds[cur_idx],
-                                              rs_callback.color_depth_ts,
-                                              tc1_callback.tc_ts,
-                                              tc2_callback.tc_ts,
+                                              cd_ts_mapped_fds[cur_idx],
+                                              tc1_ts_mapped_fds[cur_idx],
+                                              tc2_ts_mapped_fds[cur_idx],
                                               cur_idx, save_dir);
 
             post(thread_pool, save_callback);
@@ -609,16 +614,16 @@ int main() {
             }
 
             *tc1_callback.tc_mfd_ptr = tc1_mfd_ptrs[cur_idx+1];
-            tc1_callback.tc_ts = HT1_tss_vec[cur_idx+1];
+            *tc1_callback.tc_ts_mfd_ptr = tc1_ts_mfd_ptrs[cur_idx+1];
             tc1_callback.idx_tc = 0;
 
             *tc2_callback.tc_mfd_ptr = tc2_mfd_ptrs[cur_idx+1];
-            tc2_callback.tc_ts = HT2_tss_vec[cur_idx+1];
+            *tc2_callback.tc_ts_mfd_ptr = tc2_ts_mfd_ptrs[cur_idx+1];
             tc2_callback.idx_tc = 0;
 
             *rs_callback.color_mfd_ptr = color_mfd_ptrs[cur_idx+1];
             *rs_callback.depth_mfd_ptr = depth_mfd_ptrs[cur_idx+1];
-            rs_callback.color_depth_ts = color_depth_tss[cur_idx+1];
+            *rs_callback.color_depth_ts_mfd_ptr[cur_idx+1];
             rs_callback.idx_color.store(0);
             rs_callback.idx_depth = 0;
 
