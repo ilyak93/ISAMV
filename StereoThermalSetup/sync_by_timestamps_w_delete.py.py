cc798d7c9562993684dcd1a122de9f8774fa722a
IlyaKot <50303550+ilyak93@users.noreply.github.com>
2023-05-17 16:03:48 +0300

    Update sync_by_timestamps_w_delete.py.py
    - adding not needed files deletion instead of cloning all to other dir
    - less expensive with memory on hard disk

--- StereoThermalSetup/sync_by_timestamps_w_delete.py.py
+++ StereoThermalSetup/sync_by_timestamps_w_delete.py.py
@@ -19,6 +19,19 @@ def find_closest_by_ts(tc_ts_files, rs_ts_files):
                 closest_to_tc[i] = j
     return closest_to_tc
 
+def my_find_closest_by_ts(tc1_ts_files, tc2_ts_files):
+    closest_to_tc = [None] * len(tc1_ts_files)
+
+    for i in range(len(tc1_ts_files),):
+        dist = float('inf')
+        for j in range(len(tc2_ts_files)):
+            tc1_ts = int(tc1_ts_files[i].split('_')[1].split('.')[0])
+            tc2_ts = int(tc2_ts_files[j].split('_')[1].split('.')[0])
+            if abs(tc1_ts - tc2_ts) < dist:
+                dist = abs(tc1_ts - tc2_ts)
+                closest_to_tc[i] = j
+    return closest_to_tc
+
 from ctypes import wintypes, windll
 from functools import cmp_to_key
 
@@ -30,8 +43,11 @@ def winsort(data):
     cmp_fnc = lambda psz1, psz2: _StrCmpLogicalW(psz1, psz2)
     return sorted(data, key=cmp_to_key(cmp_fnc))
 
-for ll in range(1,2):
-    g_path = "G:/Vista_project/fusion/" + str(ll) + "/"
+
+
+
+for g in range(12,13):
+    g_path = "F:/Vista_project2/" + str(g)
     left_path = g_path + "/left/"
     right_path = g_path + "/right/"
     rs_path = g_path + "/rs/"
@@ -49,26 +65,38 @@ for ll in range(1,2):
     rs_files = os.listdir(rs_path)
     rs_files = winsort(rs_files)
 
+
+    #ci = my_find_closest_by_ts(left_tc_files, right_tc_files)
     closest_indices = find_closest_by_ts(left_tc_files, rs_files)
-    closest_indices2 = find_closest_by_ts(right_tc_files, rs_files)
+    valid_rs_files = [rs_files[i] for i in closest_indices]
+    files_to_delete = [file for file in rs_files if file not in valid_rs_files and "color" in file]
+    #closest_indices2 = find_closest_by_ts(right_tc_files, rs_files)
+    '''
+    for file_to_delete in files_to_delete:
+        file_name = file_to_delete[:-9]
+        color_file = file_to_delete
+        depth_file = file_name + "depth.png"
+        os.remove(rs_path+color_file)
+        os.remove(rs_path + depth_file)
+    '''
     tc_len = len(left_tc_files)
     for i in range(tc_len):
-        #tc1_np = np.round(cv2.imread(left_path + left_tc_files[i], cv2.IMREAD_UNCHANGED) / 256).astype(np.uint8)
-        #tc2_np = np.round(cv2.imread(right_path + right_tc_files[i], cv2.IMREAD_UNCHANGED) / 256).astype(np.uint8)
-        #tc_combined = np.concatenate((tc1_np, tc2_np), axis=0)
-        #im = Image.fromarray(tc_combined)
+        tc1_np = np.round(cv2.imread(left_path + left_tc_files[i], cv2.IMREAD_UNCHANGED) / 256).astype(np.uint8)
+        tc2_np = np.round(cv2.imread(right_path + right_tc_files[i], cv2.IMREAD_UNCHANGED) / 256).astype(np.uint8)
+        tc_combined = np.concatenate((tc1_np, tc2_np), axis=0)
+        im = Image.fromarray(tc_combined)
 
         rs_bgr_img = cv2.imread(rs_path + rs_files[closest_indices[i]])
         im_rgb = cv2.cvtColor(rs_bgr_img, cv2.COLOR_BGR2RGB).astype(np.uint8)
 
-        #im_depth = np.round(cv2.imread(rs_path + rs_files[closest_indices[i]+1], cv2.IMREAD_UNCHANGED) / 256).astype(np.uint8)
-        #im_depth_3ch = np.stack((im_depth, im_depth, im_depth), axis=-1)
-        #rgb_depth_combined = np.concatenate((im_rgb, im_depth_3ch), axis=0 )
-        #all_combined = np.zeros((rgb_depth_combined.shape[0], rgb_depth_combined.shape[1] + tc_combined.shape[1], 3)).astype(np.uint8)
-        #all_combined[0:rgb_depth_combined.shape[0], 0:rgb_depth_combined.shape[1], :] = rgb_depth_combined
-        #all_combined[0:tc_combined.shape[0], rgb_depth_combined.shape[1]:, :] = np.stack((tc_combined, tc_combined, tc_combined), axis=-1)
-        #im = Image.fromarray(all_combined)
-        #im.save(dest_presentation_path + str(i) + ".png", "PNG")
+        im_depth = np.round(cv2.imread(rs_path + rs_files[closest_indices[i]+1], cv2.IMREAD_UNCHANGED) / 256).astype(np.uint8)
+        im_depth_3ch = np.stack((im_depth, im_depth, im_depth), axis=-1)
+        rgb_depth_combined = np.concatenate((im_rgb, im_depth_3ch), axis=0 )
+        all_combined = np.zeros((rgb_depth_combined.shape[0], rgb_depth_combined.shape[1] + tc_combined.shape[1], 3)).astype(np.uint8)
+        all_combined[0:rgb_depth_combined.shape[0], 0:rgb_depth_combined.shape[1], :] = rgb_depth_combined
+        all_combined[0:tc_combined.shape[0], rgb_depth_combined.shape[1]:, :] = np.stack((tc_combined, tc_combined, tc_combined), axis=-1)
+        im = Image.fromarray(all_combined)
+        im.save(dest_presentation_path + str(i) + ".png", "PNG")
 
         im = Image.fromarray(im_rgb)
         im.save(dest_path + str(i) + "color.png", "PNG")
