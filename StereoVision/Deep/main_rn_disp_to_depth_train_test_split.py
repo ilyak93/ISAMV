a4b3f418573384540269cb951edc6ec0bc6a6ca6
IlyaKot <50303550+ilyak93@users.noreply.github.com>
2023-07-22 02:45:15 +0300

    add continious update for flashing drive

--- StereoVision/Deep/main_rn_disp_to_depth_train_test_split.py
+++ StereoVision/Deep/main_rn_disp_to_depth_train_test_split.py
@@ -103,6 +103,7 @@ if __name__ == '__main__':
     train_flow2_EPEs = AverageMeter()
 
     writer = SummaryWriter("/content/drive/MyDrive/Deep/")
+    
     i = 0
     j = 0
     cycles = 2
@@ -203,5 +204,8 @@ if __name__ == '__main__':
                 previous_EPE = test_flow2_EPEs.avg
                 prev_chkpnt = "/content/drive/MyDrive/Deep/" + "epoch_" + str(prev_cycles + k) + "_loss_" + str(
                     test_flow2_EPEs.avg)
+            demo = open("/content/drive/MyDrive/Deep/demofile.txt", "w")
+            demo.write("Ilya " + str(prev_cycles + k))
+            demo.close()
 
         prev_cycles = prev_cycles + FADNet_loss_config["epoches"][max(0, r - 1)]
