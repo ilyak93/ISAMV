f54886ac94b4e13283d3d737bfbcad5afda1480a
IlyaKot <50303550+ilyak93@users.noreply.github.com>
2023-07-27 22:12:05 +0300

    Update main_with_indices_train_test_split.py

--- StereoVision/Deep/main_with_indices_train_test_split.py
+++ StereoVision/Deep/main_with_indices_train_test_split.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 import matplotlib
 import torch
@@ -18,9 +19,11 @@ from torch.utils.tensorboard import SummaryWriter
 from skimage import exposure
 import numpy as np
 
+
 def histogram_equalize(img):
     img_cdf, bin_centers = exposure.cumulative_distribution(img)
     return np.interp(img, bin_centers, img_cdf).astype(np.float32)
+
 
 # tensorboard --logdir runs
 
@@ -105,19 +108,19 @@ if __name__ == '__main__':
 
     prev_chkpnt = ""
 
-    pretrain=False
+    pretrain = False
     if pretrain:
         chkp = "epoch_199_loss_1.6628163054182723"
         PATH = gen_path + chkp
         checkpoint = torch.load(PATH)
         net.load_state_dict(checkpoint['model_state_dict'])
         optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-    
+
     duration = 1800  # 1 minute
 
     # Run the script for the specified duration
     start_time = time.time()
-    
+
     for r in range(startRound, len(FADNet_loss_config["epoches"])):
         cycles = FADNet_loss_config["epoches"][r]
         for k in range(cycles):
@@ -125,7 +128,7 @@ if __name__ == '__main__':
             for [left_img, right_img], [target_disp, depths] in train_dataloader:
                 left_img = left_img.unsqueeze(dim=1) / max_uint16
                 right_img = right_img.unsqueeze(dim=1) / max_uint16
-                
+
                 actual_batch_size = left_img.size(0)
                 row_indices_feature = torch.tensor(list(range(rows))).reshape([-1, 1]).repeat(1, cols).unsqueeze(
                     0).repeat(actual_batch_size, 1, 1).unsqueeze(1) / rows
@@ -177,7 +180,7 @@ if __name__ == '__main__':
                 for [left_img, right_img], [target_disp, depths] in test_dataloader:
                     left_img = left_img.unsqueeze(dim=1) / max_uint16
                     right_img = right_img.unsqueeze(dim=1) / max_uint16
-                    
+
                     actual_batch_size = left_img.size(0)
                     row_indices_feature = torch.tensor(list(range(rows))).reshape([-1, 1]).repeat(1, cols).unsqueeze(
                         0).repeat(actual_batch_size, 1, 1).unsqueeze(1) / rows
@@ -206,27 +209,26 @@ if __name__ == '__main__':
                         #    r, i, loss=losses, flow2_EPE=flow2_EPEs))
                         writer.add_scalar("test/per_10_iterations/loss", loss.data.item(), j)
                         writer.add_scalar("test/per_10_iterations/EPE", flow2_EPE.data.item(), j)
-                        
+
                         indices = target_dis[0] != 0
                         diff = torch.zeros_like(target_dis[0]).cuda()
                         diff[indices] = torch.abs(target_dis[0][indices] - output_net2[0][indices]) / 256
-                        
 
                         diff_eq = torch.tensor(histogram_equalize(diff.cpu().numpy()))
 
                         output = torch.tensor(histogram_equalize((output_net2[0].cpu() / 256).numpy()))
-                        
+
                         label = torch.tensor(histogram_equalize((target_dis[0].cpu() / 256).numpy()))
 
                         orig_viz = torch.cat((left_img[0].cpu(),
                                               right_img[0].cpu(),
                                               output_net2[0].cpu() / 256,
                                               target_dis[0].cpu() / 256,
-                                              output, 
-                                              label, 
+                                              output,
+                                              label,
                                               diff.cpu(),
                                               diff_eq), 0).unsqueeze(1)
-                                             
+
                         grid = torchvision.utils.make_grid(orig_viz)
                         writer.add_image(tag='Test_images/image_' + str(j % 13),
                                          img_tensor=grid, global_step=prev_cycles + k,
@@ -250,9 +252,9 @@ if __name__ == '__main__':
                 prev_chkpnt = "/content/drive/MyDrive/Deep/" + "epoch_" + str(prev_cycles + k) + "_loss_" + str(
                     test_flow2_EPEs.avg)
             if time.time() - start_time >= duration:
-                  writer = SummaryWriter(gen_path)
-                  time.sleep(120)
-                  start_time = time.time()
+                writer = SummaryWriter(gen_path)
+                time.sleep(120)
+                start_time = time.time()
             demo = open("/content/drive/MyDrive/Deep/demofile.txt", "w")
             demo.write("Ilya " + str(prev_cycles + k))
             demo.close()
